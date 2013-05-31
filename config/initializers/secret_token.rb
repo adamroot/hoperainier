ff00824ec6bbe15a3cc336857338669ce4dba69a
adam <adam@hiplogiq.com>
2013-05-31 13:32:55 -0500

    untouched rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HopeRainier::Application.config.secret_token = '3f3bf58ab567891e0e3e5457e28d13d29b0025ddd4823e4673112691ff7176d15df2df8be827d0e3b3bc895141820c39fd32371cab18f98af67281b075555075'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HopeRainier::Application.config.session_store :cookie_store, key: '_hope_rainier_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HopeRainier::Application.config.session_store :active_record_store
